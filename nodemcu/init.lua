9396f9dd95a9fb5477170180eaaa6fa4aaa5bcfd
adamtomaja <adiscool@tlen.pl>
2017-08-24 17:43:03 +0200

    Connection over mqtt

--- nodemcu/init.lua
+++ nodemcu/init.lua
@@ -2,7 +2,7 @@ function onConnectToAP(connection)
     print("Connected to AP")
 end
 
-wifiConfig = {ssid = "Ave2G", pwd = "4VcDAhu2m6LZ", auto = false, connected_cb = onConnectToAP}
+wifiConfig = {ssid = "Ave2G", pwd = "", auto = false, connected_cb = onConnectToAP}
 
 wifi.sta.config(wifiConfig)
 wifi.setmode(wifi.STATION)
@@ -11,6 +11,25 @@ wifi.sta.connect()
 
 connectionChecker = tmr.create()
 
+function onConnected() 
+    m = mqtt.Client("main-client", 120)
+
+    m:on("connect", function(client) 
+        print("MQTT Connected") 
+        m:subscribe("/test",0, function(conn) print("subscribe success") end)
+    end)
+    
+    m:on("offline", function(client) print("mqtt offline") end)
+
+    m:on("message", function(client, topic, data) 
+        print("Message received")
+        print(topic)
+        print(data)
+    end)
+
+    m:connect("192.168.0.19", 8883, 0, true)
+end
+
 function checkConnection() 
     print("Checking connection...")
     if wifi.sta.status() == wifi.STA_GOTIP then
@@ -18,6 +37,9 @@ function checkConnection()
         print("IP: ")
         print(wifi.sta.getip())
         connectionChecker:stop()
+        print("hello world!")
+       
+        onConnected()
     end
 end
 
