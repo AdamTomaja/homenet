aff50f4edf7ab71371c67a1c6ffc1c97650631e1
adamtomaja <adiscool@tlen.pl>
2017-08-26 13:29:52 +0200

    Multithreaded virtual ucu

--- homenet-virtualucu/src/main/java/com/cydercode/homenet/virtualucu/StateGenerator.java
+++ homenet-virtualucu/src/main/java/com/cydercode/homenet/virtualucu/StateGenerator.java
@@ -1,11 +1,16 @@
 package com.cydercode.homenet.virtualucu;
 
 import com.cydercode.homenet.cdm.SetGpioValueMessage;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
+import javax.annotation.PostConstruct;
 import java.util.Random;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 
 @Component
 public class StateGenerator {
@@ -18,14 +23,24 @@ public class StateGenerator {
 
     private Random random = new Random();
 
-    @Scheduled(fixedRate = 200)
-    public void sendState() throws Exception {
-        SetGpioValueMessage message = new SetGpioValueMessage();
-        message.setInstanceId(instance.getID());
-        message.setPin(random.nextInt(10));
-        message.setValue(random.nextInt(2));
+    ScheduledExecutorService executor = Executors.newScheduledThreadPool(16);
 
-        messageService.sendMessage(message);
+    @PostConstruct
+    public void init() throws Exception {
+
+        executor.scheduleAtFixedRate(() -> {
+            SetGpioValueMessage message = new SetGpioValueMessage();
+            message.setInstanceId(instance.getID());
+            message.setPin(random.nextInt(10));
+            message.setValue(random.nextInt(2));
+
+            try {
+                messageService.sendMessage(message);
+            } catch (Exception e) {
+                LoggerFactory.getLogger(StateGenerator.class).error("Cannot send message", e);
+            }
+
+        }, 0, 50, TimeUnit.MILLISECONDS);
     }
 
 }
