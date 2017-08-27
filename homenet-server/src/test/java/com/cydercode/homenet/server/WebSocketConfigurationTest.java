c6c55e5e8513d86935a56f42e5fcc1ef0a39191f
adamtomaja <adiscool@tlen.pl>
2017-08-27 02:12:08 +0200

    WebSocketConfiguration Unit Test

---
+++ homenet-server/src/test/java/com/cydercode/homenet/server/WebSocketConfigurationTest.java
@@ -0,0 +1,33 @@
+package com.cydercode.homenet.server;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+
+@RunWith(MockitoJUnitRunner.class)
+public class WebSocketConfigurationTest {
+
+    @Mock
+    private WebSocketHandler webSocketHandler;
+
+    @InjectMocks
+    private WebSocketConfiguration configuration;
+
+    @Test
+    public void registerWebSocketHandler() throws Exception {
+        // given
+        WebSocketHandlerRegistry registry = mock(WebSocketHandlerRegistry.class);
+
+        // when
+        configuration.registerWebSocketHandlers(registry);
+
+        // then
+        verify(registry).addHandler(webSocketHandler, "/control-panel/ws");
+    }
+}
